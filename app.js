680f8526c095f17cc968941873067c47f9ea5176
赵海朝 <847204786@qq.com>
2018-07-27 17:19:31 +0800

    添加用户表

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('./config/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/poordb',{ useMongoClient: true });
+
+const app = express();
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+    cookie: { maxAge: 60000 },
+    secret: 'haichao',
+    saveUninitialized: false,
+    resave: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    // res.locals.success_messages = req.flash('success');
+    // res.locals.error_messages = req.flash('error');
+    res.locals.isAuthenticated = req.user ? true : false;
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({RetCode:0,RetVal:'page not found'});
+});
+
+app.listen(6000, () => console.log('Server started listening on port 6000!'));
